6b00088355710b0bc4ff6cf5c725c4bc67f62ed2
Maksim <muzyukov@icloud.com>
2021-09-17 13:26:08 +0300

    Dialogs and modified contant

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Dialogs.module.css";
+const DialogItem = (props) => {
+  return (
+    <div className={s.dialogName}>
+      <NavLink to={"/dialog/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+};
+const Message = (props) => {
+  return <div className={s.message}>{props.message}</div>;
+};
 const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div>
+        <DialogItem name="Max" id="1" />
+        <DialogItem name="John" id="2" />
+        <DialogItem name="Mary" id="3" />
+        <DialogItem name="Sara" id="4" />
+      </div>
+      <div>
+        <Message message="Hello" />
+        <Message message="How are you?" />
+        <Message message="Nice to meet you))" />
+        <Message message="Great!!" />
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
